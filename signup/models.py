bf8e28eb39b4c9da3cd50896084e8d39fbbe9a3a
PaymasterMax <duncansantiago18@gmail.com>
2020-02-19 22:07:21 -0700

    updates

--- signup/models.py
+++ signup/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class signup(models.Model):
-    pass
+    email = models.EmailField(primary_key=True , max_length = 60)
+    username = models.CharField(max_length = 30)
+    pnumber = models.IntegerField(max_length = 30)
+    location = models.CharField(max_length = 30)
+    hobby = models.CharField(max_length = 30)
+    profilepic = models.FileField()
+    password = models.CharField(max_length = 60)
 
     class Meta:
         db_table = "signup"
--- signup/views.py
+++ signup/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import signup as signmodel
 
 def signup(request):
-    return render(request , "signup/signup.html")
+    if request.method =="POST":
+        email = request.POST['email']
+        username = request.POST['username']
+        pnumber = request.POST['pnumber']
+        # location = request.POST['location']
+        hobby = request.POST['hobby']
+        profilepic = request.FILES['profilepic']
+        password = request.POST['password']
+
+        signmodel.objects.create(email = email , username = username , pnumber = pnumber ,
+        location = "Hello" , hobby = hobby , profilepic = profilepic , password = password)
+
+        return redirect("/login/")
+    else:
+        return render(request , "signup/signup.html")
